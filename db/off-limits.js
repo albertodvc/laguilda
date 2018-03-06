8bece8253dbb6d564b664d00cc7eb5ac2b6eb984
Alberto del Valle <alberto.dvc@gmail.com>
2018-03-06 22:48:32 +0100

    Clean up and update content

--- db/off-limits.js
+++ db/off-limits.js
@@ -9,14 +9,57 @@ export default {
     },
     acts: [
         {
-            title: 'Acto 1',
+            title: '#1 Intro',
+            subtitle: '_[a la BOMBA]',
             videoId: 'fCZj9_pnDgU',
-            shortDesc: 'Amebalar'
+            shortDesc: 'El sonido y la luz a modo de llamada, ayeo inicial, tarabilla o balbuceo contundente.'
         },
         {
-            title: 'Acto 2',
+            title: '#2 T/T',
+            subtitle: '_LITTLE BOY ≈ 20.000 toneladas de TNT',
             videoId: 'xpQoNCGlMgs',
-            shortDesc: 'Amebalar'
-        }
+            shortDesc: 'Donde se presentan la causa y sus efectos, el crimen que marcará el destino de C. Eatherly situándolo en una encrucijada paradójica: culpable-inocente...'
+        },
+        {
+            title: '#3 BxS',
+            subtitle: '_BREVE DESCRIPCIÓN PUNTILLOSA DE LOS HECHOS',
+            videoId: 'xpQoNCGlMgs',
+            shortDesc: 'Tal vez fue mientras sobrevolaba el Océano Pacífico en el vuelo de regreso (tras arrojar la bomba) o ya en la base de Tinian, que Eatherly comenzó a cobrar conciencia de lo acaecido.'
+        },
+        {
+            title: '#4 SG',
+            subtitle: 'PAISAJE DE ACONTECIMIENTOS',
+            videoId: 'xpQoNCGlMgs',
+            shortDesc: 'La catástrofe viene a la presencia, completa, aterrizada.  Y ya pasa, comienza a pasar.'
+        },
+        {
+            title: '#5 CT/M',
+            subtitle: 'LA LLUVIA NEGRA',
+            videoId: 'xpQoNCGlMgs',
+            shortDesc: 'Tras el paisaje de acontecimientos; su correlato, también, en el cuerpo y el espíritu de Eatherly.'
+        },
+        {
+            title: '#6 MdelaBomba',
+            videoId: 'xpQoNCGlMgs',
+            shortDesc: 'Back home ≠ Home back.  La postguerra y el olvido, el héroe de guerra que se niega a serlo, es decir, a ser un smiling hero, que se niega, pues, a olvidar.'
+        },
+        {
+            title: '#7',
+            subtitle: 'EL ETERNO RETORNO DE LOS FANTASMAS Y LOS SUEÑOS',
+            videoId: 'xpQoNCGlMgs',
+            shortDesc: 'Pero los cadáveres, ay, siguieron muriendo...'
+        },
+        {
+            title: '#8 Mandamientos de la Era atómica',
+            videoId: 'xpQoNCGlMgs',
+            shortDesc: `Relato orbital de los (primeros) Mandamientos de la Era atómica según Günther Anders:
+            Tu primer pensamiento al despertar ha de ser: ÁTOMO`
+        },
+        {
+            title: '#9 SFin',
+            subtitle: 'WACO, TEXAS',
+            videoId: 'xpQoNCGlMgs',
+            shortDesc: 'Eatherly allí, en el Manicomio para veteranos de guerra, entre el horror y el tedio, la furia y los electroshocks, el lejano reconocimiento inesperado, la afinidad electiva al fin y quizás, la fuga.'
+        },
     ]
 }
