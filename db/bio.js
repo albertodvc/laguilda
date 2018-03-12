8a2059d75a5edec037fa10469446ef394a2a3dc9
Alberto del Valle <alberto.dvc@gmail.com>
2018-03-12 20:03:57 +0100

    Bio information

---
+++ db/bio.js
@@ -0,0 +1,43 @@
+export default [
+	{
+		name: 'José Maldonado',
+		bio: 'José Maldonado es titulado en flamenco y danza española por el Conservatorio Profesional de Danza de Barcelona, su ciudad natal. A los 18 años se traslada  a Madrid donde sigue formándose con grandes maestros del baile flamenco y la danza Española como María Magdalena, Paco Romero o La China. Desde que diera el salto a la escena como profesional ha trabajado como solista o en compañías tales la Antonio Canales, Javier Latorre, Rafaela Carrasco o Rocío Molina entre muchos otros. Como coreógrafo ostenta una dilatada experiencia habiendo conseguido premios y galardones en las disciplinas de Danza Española y Flamenco en diversos certámenes nacionales. En la actualidad, tiene su propia compañía de Baile Flamenco al tiempo que colabora con diversas compañías.'
+	},
+	{
+		name: 'María M. Cabeza de Vaca',
+		bio: 'Bailarina y coreógrafa, licenciada en Filología Hispánica y titulada en ballet clásico. Formada en Danza Contemporánea en el CAD (Centro Andaluz de Danza). Desde sus inicios profesionales desarrolla su trayectoria como intérprete en compañías de danza y teatro como: “Provisional Danza” (Madrid), “La Imperdible” (Sevilla), “LaPermanente” (Sevilla), “Metros” (Barcelona), “Erre que erre” (Barcelona)... En junio del 2001 realiza su primera pieza de danza para la inauguración del Festival Internacional de Danza de Itálica, punto y partida de una dilatada carrera donde la búsqueda del dialogo entre la Danza y otras disciplinas se hace patente y denominador común.'
+	},
+	{
+		name: 'Juan Murube',
+		bio: 'Desde que empezara su carera artística en 1999, la presencian de Juan Murube como cantaor se hace casi omnipresente. Participa con su arte en multitud de espectáculos de baile Flamenco tanto en Teatros de todo el mundo como en los principales Tablaos de Flamenco de toda España. Creador de un sello personal, da el salto como solista con su primer trabajo discográfico “Bellavista” presentado en Mayo de 2016 dentro del ciclo “Cita con las Músicas” de la Fundación Cajasol. Así pues, estamos ante una figura versátil y dúctil capaz de amoldar su talento como cantaor a las necesidades expresiva de cada momento.'
+	},
+	{
+		name: 'Fernando María',
+		bio: 'La guitarra flamenca de Fernando María bebe directamente de la escuela sevillana, ciudad en la que nació y desde la que principalmente desarrolla su actividad como guitarrista. Su toque personal ha conseguido impregnarse de los sones más carismáticos del Flamenco pudiendo reconocer su gusto por los maestros de la Guitarra Flamenca. No obstante, en busca de su sello personal ha ido incorporando características de otros estilos musicales, como el gusto por la calidad del sonido de la guitarra clásica barroca, la apertura rítmica de la guitarra contemporánea latinoamericana o el estudio armónico de  la guitarra de jazz. Su actividad como guitarrista la desarrolla principalmente como acompañante, bien del cante o del baile.'
+	},
+	{
+		name: 'Isa Rodriguez',
+		bio: 'Comenzó a tocar la batería en el 2006 estudiando con profesores como David López, José Mena, Jimmy González y Antonio Coronel. Ha colaborado desde entonces en diferentes grupos del panorama sevillano y ha acompañado a artistas como la Húngara y en grabaciones y directos para el artista local Pedro Jiménez. Batería en Rocktámbulos desde 2014 hasta la fecha. Festival flamenco de Nimes en el espectáculo "Guitarra mía" acompañando a artistas de la talla de Rocío Molina de la mano de su padre el guitarrista flamenco Rafael Rodríguez. Espectáculo "Off Limits" en el festival de danza de Itálica, julio 2017.'
+	},
+	{
+		name: 'Shaula Ortega',
+		bio: 'Licenciada en Bellas Artes, la trayectoria artística de Shaula Ortega va ligada indefectiblemente al desarrollo digital y las numerosas posibilidades audiovisuales. Su trabajo entabla un dialogo directo entre la naturaleza de la existencia humana y la complejidad de las estructuras sociales. Por ello, al tiempo que desarrolla su labor como artista visual independiente, ha formado parte de Compañías de Baile Flamenco y Danza así como de espectáculos y eventos musicales de toda orden.'
+	},
+	{
+		name: 'Enrique López de Haro',
+		bio: 'Realizó estudios de Escenografía, Dirección Escénica y Arquitectura en Sevilla, su ciudad natal, Berlín y Barcelona, disciplinas que sigue compaginando.  Actualmente desarrolla su actividad escénica en laGuilda Obscénica cuya primera producción ha sido Offlimits.'
+	},
+	{
+		name: 'Alberto del Valle',
+		bio: 'Ingeniero de sonido y desarrollador de software, ha participado en multitud de producciones audiovisuales combinando producción discográfica, espectáculos musicales, danza, cine, television. Un indígena digital que retuerce botones disfrutando con cualquier reto técnico sin asustarle, lidiar con “artistas” y otras criaturas salvajes. Se puede decir que se dedica a hacer cosas, cosas con cacharros y a veces con otros seres humanos.'
+	},
+	{
+		name: 'Juan Monje',
+		bio: 'Nacido entre la provincia y la periferia de Sevilla. Filósofo, gestor y técnico de desarrollo. Promotor de proyectos independientes, antiguas y nuevas tendencias en todo tipo de soportes y medios. Admirador de todo lo relativo al vivir flamenco.'
+	},
+	{
+		name: 'Isabel Arias de Saavedra',
+		bio: 'Realizó estudios de interpretación, patronaje y diversos cursos de vestuario teatral y técnicas textiles, entre Sevilla, Madrid y Granada. Aunando todos sus estudios, hoy en día se dedica a: sastrería y vestuario teatral en compañías andaluzas de danza y/o teatro, profesora de costura en el taller La Osa Mallol y creadora de Hisabelia, marca textil de ropa y complementos con el lema Slow design for happy people.'
+	}
+
+]
