2c135324218f30a751de31a8f2f27c5a6b49f46b
Alberto del Valle <alberto.dvc@gmail.com>
2018-03-13 09:24:41 +0100

    Adds credits

---
+++ db/credits.js
@@ -0,0 +1,17 @@
+export default [
+	{key: 'Idea y guión original', value: 'Enrique y Fernando López de Haro '},
+	{key: 'Dirección de escena', value: 'Enrique López de Haro '},
+	{key: 'Dirección musical y composición', value: 'Fernando María'},
+	{key: 'Coreografías', value: 'María Cabeza de Vaca y José Maldonado'},
+	{key: 'Bailarines', value: 'José Maldonado y María Cabeza de Vaca'},
+	{key: 'Cante', value: 'Juan Murube'},
+	{key: 'Guitarras', value: 'Fernando María'},
+	{key: 'Batería', value: 'Isa Rodríguez'},
+	{key: 'Programación musical', value: 'Fernando María y Alberto del Valle'},
+	{key: 'Montajes y Proyecciones', value: 'Shaula Ortega'},
+	{key: 'Vestuario', value: 'Hisabela'},
+	{key: 'Fotografía', value: 'Luís Ollero'},
+	{key: 'Grabación', value: 'Zancada Films'},
+	{key: 'Producción Ejecutiva', value: 'Fernando López de Haro'},
+	{key: 'Producción', value: 'Juan Monje'}
+]
